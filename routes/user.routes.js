8d7a1ce7d289b6df3630c83a798e6f7aba5f14e6
Dylan <dyl.blink.1.8.2@gmail.com>
2022-08-15 20:19:47 -0500

    user routes complete :poop

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,21 @@
 const router = require("express").Router();
 //importar el controlador 
-const {getLoggedUser,editProfile} = require("../controllers/user.controller")
+const {getLoggedUser,editProfile,getUserById,onlyAdminRead, deleteAccount} = require("../controllers/user.controller")
 //vamos a importar los middleware 
-const { verifyToken } = require("../middleware")
+const { verifyToken,checkRole } = require("../middleware")
 
 //Read - perfil
 router.get("/my-profile",verifyToken,getLoggedUser);
 //UPDate - Perfil
 router.patch("/edit-profile",verifyToken,editProfile);
 //Delete - user
-//router.delete("/delete-user");
+router.delete("/delete-user",verifyToken, deleteAccount);
 
 //Read - otro usuario
-//router.get("/:id/profile")
+router.get("/:id/profile",verifyToken,getUserById);
+
+//REad all user!!! (ADMIN STAFF)
+router.get("/admin/users",verifyToken, checkRole( ["Admin"] ), onlyAdminRead  )
 
 
 module.exports = router;
